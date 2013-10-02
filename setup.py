f895fa51531f97f5be48f7f3463c1c68d7e0fa1b
Dan Loewenherz <dloewenherz@gmail.com>
2013-10-02 10:33:14 -0500

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name = 'django-lionheart-helpers',
-    version = '1.0',
+    version = '1.0.1',
     url = 'http://lionheartsw.com',
     license = 'BSD',
     description = 'Django decorators and some other utilities.',

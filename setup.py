44e0b98318d7c28026da5c0b60e99dcc348fb8f7
Dan Loewenherz <dloewenherz@gmail.com>
2013-12-03 10:49:42 -0600

    bump version number to 1.0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name = 'django-lionheart-helpers',
-    version = '1.0.3',
+    version = '1.0.4',
     url = 'http://lionheartsw.com',
     license = 'BSD',
     description = 'Django decorators and some other utilities.',

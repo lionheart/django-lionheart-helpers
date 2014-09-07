304715a3068ba28d17ad8ebc32f75d5fb646e7b7
Dan Loewenherz <dloewenherz@gmail.com>
2014-09-07 18:44:19 -0500

    add validators

---
+++ lionheart/validators.py
@@ -0,0 +1,35 @@
+import re
+from django import forms
+
+zip_format = re.compile(r'^\d{5}$')
+zip_plus_four_format = re.compile(r'^\d{5}-\d{4}$')
+
+def zip_code_validator(value):
+    if not zip_format.match(value) \
+            and not zip_plus_four_format.match(value):
+        raise forms.ValidationError("Zip code is improperly formatted.")
+
+def phone_validater(value):
+    non_numbers = re.compile(r'[^\d]')
+    value = non_numbers.sub('', value)
+
+    if not 9 < len(value) < 16:
+        message = "Phone number must be between 10 to 15 digits."
+        raise forms.ValidationError(message)
+
+    return value
+
+def po_box_validator(value, message):
+    """
+    Matches the following strings:
+
+    * PO Box 1234
+    * P.O. Box 1234
+    * P.O Box 1234
+    * Po 1234
+    * po 1234
+    """
+
+    if po_box_format.search(value):
+        raise forms.ValidationError(message)
+

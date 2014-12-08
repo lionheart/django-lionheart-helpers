2f2eb7030bbd21cd60fc0dceaf86ca1328d0111f
Dan Loewenherz <dloewenherz@gmail.com>
2014-12-08 14:16:15 -0600

    import metadata in init file

---
+++ lionheart/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""
+Django Statictastic
+~~~~~~~~~~~~~~~~~~~
+
+:copyright: © 2014 Lionheart Software
+"""
+
+from .metadata import (
+    __author__,
+    __copyright__,
+    __email__,
+    __license__,
+    __maintainer__,
+    __version__,
+)
+
+__all__ = [
+    '__author__', '__copyright__', '__email__', '__license__',
+    '__maintainer__', '__version__'
+]
+

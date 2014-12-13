8f8d892718c15f1a8fc5b643c3a2d58cc83e4a9f
Dan Loewenherz <dloewenherz@gmail.com>
2014-12-13 16:54:57 -0600

    don't import in __init__.py, refs #2

--- lionheart/__init__.py
+++ lionheart/__init__.py
@@ -16,31 +16,8 @@ from .metadata import (
     __version__,
 )
 
-from django.conf import settings
-if not settings.configured:
-    settings.configure()
-
-import decorators
-import forms
-import models
-import settings
-import storages
-import urls
-import utils
-import validators
-import views
-
 __all__ = [
     '__author__', '__copyright__', '__email__', '__license__',
     '__maintainer__', '__version__'
-    'decorators',
-    'forms',
-    'models',
-    'settings',
-    'storages',
-    'urls',
-    'utils',
-    'validators',
-    'views'
 ]
 
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env/python
 # -*- coding: utf-8 -*-
 
-import metadata
+from lionheart import metadata
 
 try:
     from setuptools import setup

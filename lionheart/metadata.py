6a0e751f4b9b161ffe05b0b397a4d36668ec8d03
Dan Loewenherz <dloewenherz@gmail.com>
2015-04-12 16:34:23 -0500

    bump version to 1.2

--- lionheart/metadata.py
+++ lionheart/metadata.py
@@ -1,6 +1,6 @@
-__version__ = "1.1.15"
+__version__ = "1.2"
 __author__ = "Dan Loewenherz"
-__copyright__ = "Copyright 2014, Lionheart Software"
+__copyright__ = "Copyright 2015, Lionheart Software"
 __maintainer__ = "Dan Loewenherz"
 __email__ = "dan@lionheartsw.com"
 __license__ = "Apache 2.0"

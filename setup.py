50630597c08127f6d77ef4a997b40ca3f2b1baf8
Dan Loewenherz <dan@lionheartsw.com>
2016-07-30 09:35:10 -0500

    update licensing and metadata distribution

--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env/python
 # -*- coding: utf-8 -*-
 
+# Copyright 2015 Lionheart Software LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from lionheart import metadata
 
 try:
@@ -8,14 +22,18 @@ try:
 except ImportError:
     from distutils.core import setup
 
+metadata = {}
+metadata_file = "lionheart/metadata.py"
+exec(compile(open(metadata_file).read(), metadata_file, 'exec'), metadata)
+
 setup(
     name='django-lionheart-helpers',
-    version=metadata.__version__,
+    version=metadata['__version__'],
     url='http://lionheartsw.com',
-    license=metadata.__license__,
+    license=metadata['__license__'],
     description='Django decorators and some other utilities.',
-    author=metadata.__author__,
-    author_email=metadata.__email__,
+    author=metadata['__author__'],
+    author_email=metadata['__email__'],
     packages=['lionheart']
 )
 

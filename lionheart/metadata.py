44f2a58d7e1bf242d71d066bbe7226328d23ca59
Dan Loewenherz <dan@lionheartsw.com>
2017-08-12 15:43:36 -0500

    update copyright dates

--- lionheart/metadata.py
+++ lionheart/metadata.py
@@ -1,6 +1,6 @@
 __version__ = "3.0.0"
 __author__ = "Dan Loewenherz"
-__copyright__ = "Copyright 2015, Lionheart Software"
+__copyright__ = "Copyright 2015-2017, Lionheart Software"
 __maintainer__ = "Dan Loewenherz"
 __email__ = "dan@lionheartsw.com"
 __license__ = "Apache 2.0"
